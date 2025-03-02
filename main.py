3863e916d178a8660fac8af0f0734ba9df181dd1
Donovan <commit@dkrm.dev>
2025-03-02 18:49:47 +0100

    run at 60 fps

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +16,8 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
 
 
 if __name__ == "__main__":
