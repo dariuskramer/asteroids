e330b26a4904fb76280aea008892df87e61c8d38
Donovan <commit@dkrm.dev>
2025-03-04 14:56:42 +0100

    add player, asteroids and collision detection

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = updatables
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatables, drawables)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -14,10 +29,21 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        updatables.update(dt)
+
+        for asteroid in asteroids:
+            if player.collide(asteroid):
+                print("Game Over!")
+                return
+
+        screen.fill("black")
+
+        for drawable in drawables:
+            drawable.draw(screen)
+
         pygame.display.flip()
-        delta_time = clock.tick(60)
-        dt = delta_time / 1000
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
